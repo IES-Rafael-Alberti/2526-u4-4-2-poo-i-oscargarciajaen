e7ab21b6f6c801732c846c9915acf84ca43b9dd7
oscargarcia <ogarjae565@g.educaand.es>
2026-01-17 19:44:18 +0100

    ejercicio Rectangulo realizado

---
+++ src/main/kotlin/Rectangulo.kt
@@ -0,0 +1,48 @@
+/*
+    Crear una clase Rectángulo, con atributos base y altura. La clase debe disponer del constructor y 
+    los métodos para calcular el area y el perimetro. Los atributos no se podrán modificar, aunque si consultar. 
+    Por último, tendrán que ser mayor que 0.
+
+    Opcionalmente se puede crear el método toString() para mostrar información sobre el 
+    rectángulo: override fun toString() = "". (Pulsa Ctrl+o)
+
+    En el programa principal, crear varios rectángulos. Mostarlos y mostrar por pantalla sus áreas y perímetros.
+ */
+
+class Rectangulo(val base:Double, val altura:Double){
+
+    init {
+        require(altura > 0) { "La altura debe ser mayor a 0" }
+        require(base > 0) { "La base debe ser mayor a 0" }
+    }
+
+    override fun toString(): String = "El rectangulo tiene una base de ${this.base} y una altura de ${this.altura} "
+
+    fun calcularArea():Double{
+        val area: Double = base * altura
+        println(area)
+        return area
+    }
+
+    fun calcularPerimetro():Double{
+        val perimetro: Double = (base * 2) + (altura * 2)
+        println(perimetro)
+        return perimetro
+    }
+}
+
+fun main(){
+    try {
+        val rectangulo1 = Rectangulo(6.0, 2.0)
+        val rectangulo2 = Rectangulo(8.0, 6.0)
+        val rectangulo3 = Rectangulo(2.0, 1.0)
+        println(rectangulo1)
+        println(rectangulo2)
+        println(rectangulo3)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+
+}
+
+
